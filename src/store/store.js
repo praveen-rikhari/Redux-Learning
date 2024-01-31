83fbb25ac0ddd481e2c0956dad36f98608656671
praveen-rikhari <praveenrikhari45@gmail.com>
2024-01-31 15:32:26 +0530

    updated store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../reducer/counterSlice";
 import quoteReducer from "../reducer/quoteSlice";
+import calculatorReducer from "../reducer/calculatorSlice";
+import myMiddleware from "../middleware/myMiddleware";
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
         quote: quoteReducer,
+        calculator: calculatorReducer,
     },
+    middleware:
+        (getDefaultMiddleware) => getDefaultMiddleware().concat(myMiddleware),
 });
+
+export default store;
